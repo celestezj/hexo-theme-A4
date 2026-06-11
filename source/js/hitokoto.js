3ed51b8562ed6179732dda3f765609bbf9f90bb1
muggledy <zgjsycfndy2015@163.com>
2026-06-12 00:07:42 +0800

    fix a bug of hitoko for pjax scene

--- source/js/hitokoto.js
+++ source/js/hitokoto.js
@@ -1,14 +1,27 @@
-fetch('https://v1.hitokoto.cn/?c=k') // 具体接口信息：https://developer.hitokoto.cn/sentence/
-    .then(function (res) {
-        return res.json();
-    })
-    .then(function (data) {
-        var hitokoto = document.getElementById('quote')
-        hitokoto.innerText = data.hitokoto;
-    })
-    .catch(function (err) {
-        console.error(err);
-    })
+// 防重复请求锁
+let isLoading = false;
 
+function getHitokoto() {
+  if (isLoading) return;
+  const quote = document.getElementById('quote');
+  if (!quote) return;
 
+  isLoading = true;
+  quote.innerText = "";
 
+//   console.log("get index.hitokoto");
+  fetch(`https://v1.hitokoto.cn/?c=k&_t=${Date.now()}`) // 具体接口信息：https://developer.hitokoto.cn/sentence/
+    .then(res => res.json())
+    .then(data => {
+      if (quote) quote.innerText = data.hitokoto;
+    })
+    .catch(err => {
+    //   console.error(err);
+      if (quote) quote.innerText = "一言加载失败";
+    })
+    .finally(() => {
+      isLoading = false;
+    });
+}
+
+document.addEventListener('DOMContentLoaded', getHitokoto);
