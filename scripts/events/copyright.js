0de8f450fda305efd30e32fa027160c153ca96fa
muggledy <zgjsycfndy2015@163.com>
2026-09-20 20:06:34 +0800

    fix a bug of post copyright

--- scripts/events/copyright.js
+++ scripts/events/copyright.js
@@ -2,7 +2,10 @@ var fs = require('fs');
 var path = require('path');
 
 // 参照：https://blog.d77.xyz/archives/1dc407.html
-hexo.extend.filter.register('before_post_render', function(data){
+// 必须在 after_post_render 注入：此时 content 已是最终 HTML，
+// 若在 before_post_render（原始 markdown）阶段拼接版权块，
+// 当文章结尾是列表/代码块且无空行时，版权 <blockquote> 会被误当作列表项延伸而缩进。
+hexo.extend.filter.register('after_post_render', function(data){
 
     // 只对文章生效
     if (data.layout !== 'post') return data;
